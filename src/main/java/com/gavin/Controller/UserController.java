cd63b6c2be22f7208556f79f2e359c8ddc82af25
gavin <1187342818@qq.com>
2021-11-22 10:28:51 +0800

    学习笔记,好像只有我这么干的吧

---
+++ src/main/java/com/gavin/Controller/UserController.java
@@ -0,0 +1,5 @@
+package com.gavin.Controller;
+//用于定义业务逻辑
+public interface UserController {
+    public void findByName();
+}
---
+++ src/main/java/com/gavin/Service/UserService.java
@@ -0,0 +1,5 @@
+package com.gavin.Service;
+
+public interface UserService {
+    public void findPer();
+}
